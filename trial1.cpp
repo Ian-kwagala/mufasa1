c61b866d418988af954796afc45682177fbd03ab
Ian-kwagala <iankwagala2022@gmail.com>
2023-11-07 18:49:26 +0300

    mufasa

---
+++ trial1.cpp
@@ -0,0 +1,25 @@
+//Program to find the total of three numbers
+#include <iostream>
+//using the name spaces
+using namespace std;
+int main()
+{
+int num1, num2, num3, total;
+
+//Enter the first number
+std::cout<<"Enter the first number:";
+cin>>num1;
+//Enter the second number
+std::cout<<"Enter the second number:";
+std::cin>>num2;
+//Enter the thrid number
+std::cout<<"Enter the thrid number:";
+std::cin>>num3;
+//Add the three numbers
+total=num1+num2+num3;
+//Display the total of the three numbers
+std::cout<<"The total of the three numbers:"<<total<<endl;
+std::cout<<"fxxxxxxk u";
+//End of the program
+ return 0;
+}
---
+++ trial2.cpp
@@ -0,0 +1,8 @@
+//Program to say hello
+#include <iostream>
+using namespace std; 
+int main()
+{
+
+    return 0;
+}
